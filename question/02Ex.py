958d1d46870126d14f5e29679d4c22532f508908
geyang <302756880@qq.com>
2018-02-01 14:07:54 +0800

    update some questions

--- question/02Ex.py
+++ question/02Ex.py
@@ -80,6 +80,7 @@ print(list2)
 '''
 # 题目19：一个5位数，判断它是不是回文数。即12321是回文数，个位与万位相同，十位与千位相同。
 # 增加条件：相邻位不相同
+'''
 list3 = []
 for index in range(10000, 100000):
     one = index % 10
@@ -91,3 +92,4 @@ for index in range(10000, 100000):
         list3.append(index)
 
 print(list3)
+'''
